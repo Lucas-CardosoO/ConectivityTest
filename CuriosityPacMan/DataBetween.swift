3077cddaf8a85906e10a687744b7bc7a9cb2f3dd
vinicius emanuel <vncemanuel@gmail.com>
2019-07-27 17:06:15 -0300

    file general

---
+++ CuriosityPacMan/DataBetween.swift
@@ -0,0 +1,11 @@
+//
+//  DataBetween.swift
+//  CuriosityPacMan
+//
+//  Created by vinicius emanuel on 27/07/19.
+//  Copyright © 2019 Lucas Cardoso. All rights reserved.
+//
+
+import Foundation
+
+let serviceType = "hws-kb"
