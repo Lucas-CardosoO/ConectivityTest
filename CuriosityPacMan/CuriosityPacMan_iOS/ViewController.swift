13daa743cd185f23ac9d62acfd42a8bed56e4d8e
vinicius emanuel <vncemanuel@gmail.com>
2019-07-27 16:54:27 -0300

    add ios target

---
+++ CuriosityPacMan/CuriosityPacMan_iOS/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  CuriosityPacMan_iOS
+//
+//  Created by vinicius emanuel on 27/07/19.
+//  Copyright © 2019 Lucas Cardoso. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class ViewController: UIViewController {
+    
+    lazy var peerID = MCPeerID(displayName: UIDevice.current.name)
+    lazy var mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .optional)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: MCSessionDelegate, MCBrowserViewControllerDelegate{
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+            
+        case MCSessionState.connecting:
+            print("Connecting: \(peerID.displayName)")
+            
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
